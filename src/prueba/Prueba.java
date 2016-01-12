e9e272ad824aa2c08f12e905d5c9085334664d9d
secaor1516daw2 <secaor1516daw2@gmail.com>
2016-01-12 03:26:05 +0100

    p1

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,38 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author secaor1516daw2@gmail.com
+ */
+import java.io.*;
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+public static void main(String[] args) throws IOException {
+        File f = new File("fitxer.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");bw.newLine();
+            bw.write("  <head>");bw.newLine();
+            bw.write("    <title>");bw.newLine();
+            bw.write("      Nova p&agrave;gina  web");bw.newLine();
+            bw.write("    </title>");bw.newLine();
+            bw.write("  </head>");bw.newLine();
+            bw.write("  <body>");bw.newLine();
+            bw.write("    <h1>DAW2 m08uf4pr2</h1>");bw.newLine();
+            bw.write("    Nova p&agrave;gina web");bw.newLine();
+            bw.write("  </body>");bw.newLine();
+            bw.write("</html>");bw.newLine();
+            bw.close();
+        }
+    }
+    
+}
